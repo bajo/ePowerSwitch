bad29e6545ebca13f5ba61eec10bf2062e0e65d3
Markus Bajones <markus.bajones@gmail.com>
2014-12-23 21:50:30 +0100

    Switch from httplib2 to urllib2
    - httplib2 is not available on openelec
    - made adjustements for urllib2

--- ePowerSwitch.py
+++ ePowerSwitch.py
@@ -8,114 +8,138 @@ Tested on ePowerSwitch-4.
 """
 
 import base64
-import httplib2
 import string
 import re
-import prettytable
 import time
+import urllib
+import urllib2
+import sys
+try:
+    import prettytable
+except:
+    print('No pretty output possible')
+    pretty = None
 
 class EpsSocket:
-	""" class for ePowerSwitch power outlets """
-	""" each EpsSocket has a number, name, status and an unknown parameter """
-	def __init__(self, number, name, status, param, cycles):
-		self.number = number
-		self.name = name
-		self.status = status
-		self.param = param
-		self.cycles = cycles
-	
-	number = 0
-	name = ''
-	status = 0
-	param = 0
-	cycles = 0
+    """
+    class for ePowerSwitch power outlets
+    each EpsSocket has a number, name, status and an unknown parameter 
+    """
+    def __init__(self, number, name, status, param, cycles):
+        self.number = number
+        self.name = name
+        self.status = status
+        self.param = param
+        self.cycles = cycles
+    
+    number = 0
+    name = ''
+    status = 0
+    param = 0
+    cycles = 0
 
 class EpsSwitch:
-	""" class for ePowerSwitch power outlets """
-	""" each EPowerSwitch has 1, 2, 4 or 8 EpsSockets, name, ip, user and pw """
+    """
+    class for ePowerSwitch power outlets
+    each EPowerSwitch has 1, 2, 4 or 8 EpsSockets, name, ip, user and pw
+    """
 
-	def __init__(self, ip, port, user, password):
-		self.port = port
-		self.url = 'http://'+ip+':'+self.port+'/'
-		self.user = user
-		self.password = password
-		self.auth = base64.encodestring(self.user + ':' + self.password)
-		self.h = httplib2.Http()
-		self.header = { 'Authorization' : 'Basic ' + self.auth }
+    def __init__(self, ip, port, user, password):
+        self.port = str(port)
+        self.url = 'http://'+ip+':'+self.port+'/'
+        self.user = user
+        self.password = password
+        base64string = base64.encodestring(
+                        '%s:%s' % (self.user, self.password))[:-1]
+        self.authheader =  "Basic %s" % base64string
 
-	name = ''
-	port = '80'
-	url = ''
-	sockets = []
-	user = ''
-	password = ''
-	auth = ''
-	h = ''
-	header = ''
+    name = ''
+    port = '80'
+    url = ''
+    sockets = []
+    user = ''
+    password = ''
 
-	def getData(self):
-		""" getData function """
-		""" retrieve data from ePowerSwitch power outlet """
-		""" data is the name, socketname, status and count of power cycles """
+    def getData(self):
+        """
+        getData function
+        retrieve data from ePowerSwitch power outletd
+        data is the name, socketname, status and count of power cycles 
+        """
 
-		del self.sockets[:]
-		resp, content = self.h.request(self.url+'config/home_f.html', 'GET', headers=self.header)
-		resp, content = self.h.request(self.url+'cmd.html', 'GET', headers=self.header)
-		if content == '401 Authorization Required':
-			print 'Webserver is too slow. \nPlease repeat the request'
-			return False
-		else:
-			cycles = dict()
-			resp2, content2 = self.h.request(self.url+'config/misc_f.html', 'GET', headers=self.header)
-			if content2 == '401 Authorization Required':
-				print 'user '+self.user+' is not the administration user.\n Power cycle count will not be available'
-			else:
-				lines2 = string.split(content2, '\n')
-				sockets2 = [s for s in lines2 if "Socket" in s]
-				for item in sockets2:
-					a = re.search(r"Socket(.+)\)",item)
-					b = a.group(1).split(',')[0].split('"')[0].strip()
-					c = a.group(1).split(',')[1].strip()
-					cycles[b] = c
+        del self.sockets[:]
+        content = None
+        #content = self.getData2(self.url+'cmd.html')
+        get_req =  urllib2.Request(self.url+'cmd.html')
+        get_req.add_header("Authorization", self.authheader)
+        handle = urllib2.urlopen(get_req)
+        try:
+            content = handle.read()
+        except:
+            print 'cmd.html ERROR'
+        if content == '401 Authorization Required':
+            print 'Webserver is too slow. \nPlease repeat the request'
+            return False
+        else:
+            cycles = dict()
+            content2 = None
+            try:
+                get_req =  urllib2.Request(self.url+'config/misc_f.html')
+                get_req.add_header("Authorization", self.authheader)
+                handle = urllib2.urlopen(get_req)
+                content2 = handle.read()
+            except IOError, e:
+                print 'config/misc_f.html ERROR'
+            if content2:
+                lines2 = string.split(content2, '\n')
+                sockets2 = [s for s in lines2 if "Socket" in s]
+                for item in sockets2:
+                    a = re.search(r"Socket(.+)\)",item)
+                    b = a.group(1).split(',')[0].split('"')[0].strip()
+                    c = a.group(1).split(',')[1].strip()
+                    cycles[b] = c
 
-			lines = string.split(content, '\n')
-			tmp_name = [s for s in lines if "H1" in s]
-			sockets = [s for s in lines if "socket(" in s]
-			self.name = re.search(r"<H1>(.+)<\/H1>",tmp_name[0]).group(1).strip()
+            lines = string.split(content, '\n')
+            tmp_name = [s for s in lines if "H1" in s]
+            sockets = [s for s in lines if "socket(" in s]
+            self.name = re.search(r"<H1>(.+)<\/H1>",tmp_name[0]).group(1).strip()
 
-			for socket in sockets:
-				item = re.search(r"\((.+)\)",socket).group(1).strip()
-				number = int(item.split(',')[0]) 
-				name = item.split(',')[1].split()[0][1:]
-				status = int(item.split(',')[2]) 
-				param = int(item.split(',')[3]) 
-				if len(cycles) < number:
-					cycles[str(number)] = 'None'
-				self.sockets.append(EpsSocket(number, name, status, param, cycles[str(number)]))
-			return True
+            for socket in sockets:
+                item = re.search(r"\((.+)\)",socket).group(1).strip()
+                number = int(item.split(',')[0]) 
+                name = item.split(',')[1].split()[0][1:]
+                status = int(item.split(',')[2]) 
+                param = int(item.split(',')[3]) 
+                if len(cycles) < number:
+                    cycles[str(number)] = 'None'
+                self.sockets.append(EpsSocket(number, name, status, param, cycles[str(number)]))
+            return True
 
-	def showStatus(self):
-		""" print status of all sockets for the ePowerSwitch """
-		if (self.getData()):
-			print self.name
-			x = prettytable.PrettyTable(['socket number', 'connected device', 'status', 'power cycles'])
-			for socket in self.sockets:
-				if socket.status: 
-					status = 'On'
-				else: 
-					status = 'Off'
-				x.add_row([socket.number, socket.name, status, socket.cycles])
-			print x
-	
-	def setStatus(self, number, status):
-		""" set status of socket """
-		if status.lower() in {'on','On','1'}:
-			name = 'P'+str(number)+'=1'
-		elif status.lower() in {'off','Off','0'}:
-			name = 'P'+str(number)+'=0'
-		else: 
-			return False
-		resp, content = self.h.request(self.url+'cmd.html', 'POST', headers=self.header, body=name)
-		time.sleep(1)
-		resp, content = self.h.request(self.url+'cmd.html', 'POST', headers=self.header, body=name)
-		return True
+    def showStatus(self):
+        """ print status of all sockets for the ePowerSwitch """
+        if (self.getData()):
+            print self.name
+            x = prettytable.PrettyTable(['socket number', 'connected device', 'status', 'power cycles'])
+            for socket in self.sockets:
+                if socket.status: 
+                    status = 'On'
+                else: 
+                    status = 'Off'
+                x.add_row([socket.number, socket.name, status, socket.cycles])
+            print x
+    
+    def setStatus(self, number, status):
+        """ set status of socket """
+        if status.lower() in {'on','1'}:
+            data = urllib.urlencode({'P'+str(number) : '1'})
+        elif status.lower() in {'off','0'}:
+            data = urllib.urlencode({'P'+str(number) : '0'})
+        else: 
+            return False
+        set_req =  urllib2.Request(self.url+'cmd.html', data)
+        set_req.add_header("Authorization", self.authheader)
+        try:
+            response = urllib2.urlopen(set_req)
+        except:
+            response = urllib2.urlopen(set_req)
+        return True
